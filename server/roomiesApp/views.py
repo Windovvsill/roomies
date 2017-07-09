a9ae0425c61f1af189cac84871623dc592973ac0
Steve Vezeau <svezer@live.ca>
2017-07-09 16:30:51 -0700

    move django files

--- server/roomiesApp/views.py
+++ server/roomiesApp/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def test(request):
+    return HttpResponse(rr(5))
+
+def rr(n):
+    return
+
+
